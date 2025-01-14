85e13fec261d1c6b57b30bb139887ed30cdbe908
nicolaskjh <nicolas.koh.jh@gmail.com>
2025-01-15 00:22:15 +0800

    Added greeting and exit

---
+++ src/main/java/Luna.java
@@ -0,0 +1,10 @@
+public class Luna {
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------");
+        System.out.println("Hello! I'm Luna!");
+        System.out.println("What can I do for you?");
+        System.out.println("-------------------------------------");
+        System.out.println("Hope to see you again soon! meow");
+        System.out.println("-------------------------------------");
+    }
+}
