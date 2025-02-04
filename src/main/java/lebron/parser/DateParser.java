f6751af1ecf7bdb497f9c04b3b9ed474cf3a2c2f
nicolaskjh <nicolas.koh.jh@gmail.com>
2025-02-04 21:29:13 +0800

    Add date parser

---
+++ src/main/java/lebron/parser/DateParser.java
@@ -0,0 +1,4 @@
+package lebron.parser;
+
+public class DateParser {
+}
--- src/main/java/lebron/parser/InputParser.java
+++ src/main/java/lebron/parser/InputParser.java
@@ -1,18 +1,18 @@
-package cheeto.reader;
+package lebron.reader;
 
-import cheeto.CheetoException;
+import lebron.LebronException;
 
-import cheeto.command.AddCommand;
-import cheeto.command.Command;
-import cheeto.command.DeleteCommand;
-import cheeto.command.ExitCommand;
-import cheeto.command.ListCommand;
-import cheeto.command.MarkDoneCommand;
-import cheeto.command.UnmarkDoneCommand;
+import lebron.command.AddCommand;
+import lebron.command.Command;
+import lebron.command.DeleteCommand;
+import lebron.command.ExitCommand;
+import lebron.command.ListCommand;
+import lebron.command.MarkDoneCommand;
+import lebron.command.UnmarkDoneCommand;
 
-import cheeto.task.Deadline;
-import cheeto.task.Event;
-import cheeto.task.Todo;
+import lebron.task.Deadline;
+import lebron.task.Event;
+import lebron.task.Todo;
 
 public class InputReader {
     private static final String PREFIX_EXIT = "bye";
@@ -27,7 +27,7 @@ public class InputReader {
     private static final String SPLIT_DEADLINE = "/by";
     private static final String SPLIT_EVENT = "/";
 
-    public static Command readInput(String input) throws CheetoException {
+    public static Command readInput(String input) throws LebronException {
         if (input.startsWith(PREFIX_EXIT)) {
             return new ExitCommand();
         } else if (input.startsWith(PREFIX_LIST)) {
@@ -45,17 +45,17 @@ public class InputReader {
         } else if (input.startsWith(PREFIX_DELETE)) {
             return readDeleteInput(input);
         } else {
-            throw new CheetoException("I'm not sure what you mean by this meow :(");
+            throw new LebronException("I'm not sure what you mean by this meow :(");
         }
     }
 
-    public static Command readTodoInput(String input) throws CheetoException {
+    public static Command readTodoInput(String input) throws LebronException {
         String[] split = input.split(" ", 2);
 
         if (split.length != 2) {
-            throw new CheetoException("I need a description for your todo task!");
+            throw new LebronException("I need a description for your todo task!");
         } else if (split[1].trim().isEmpty()) {
-            throw new CheetoException("I need a description for your todo task!");
+            throw new LebronException("I need a description for your todo task!");
         }
 
         return new AddCommand(new Todo(split[1].trim()));
@@ -75,37 +75,37 @@ public class InputReader {
         return new AddCommand(new Event(eventSplit[0].trim(), eventSplit[1].trim(), eventSplit[2].trim()));
     }
 
-    public static Command readMarkInput(String input) throws CheetoException {
+    public static Command readMarkInput(String input) throws LebronException {
         String[] split = input.split(" ", 2);
 
         if (split.length != 2) {
-            throw new CheetoException("I need a task number to mark!");
+            throw new LebronException("I need a task number to mark!");
         } else if (split[1].trim().isEmpty()) {
-            throw new CheetoException("I need a task number to mark!");
+            throw new LebronException("I need a task number to mark!");
         }
 
         return new MarkDoneCommand(Integer.parseInt(split[1]) - 1);
     }
 
-    public static Command readUnmarkInput(String input) throws CheetoException {
+    public static Command readUnmarkInput(String input) throws LebronException {
         String[] split = input.split(" ", 2);
 
         if (split.length != 2) {
-            throw new CheetoException("I need a task number to unmark!");
+            throw new LebronException("I need a task number to unmark!");
         } else if (split[1].trim().isEmpty()) {
-            throw new CheetoException("I need a task number to unmark!");
+            throw new LebronException("I need a task number to unmark!");
         }
 
         return new UnmarkDoneCommand(Integer.parseInt(split[1]) - 1);
     }
 
-    public static Command readDeleteInput(String input) throws CheetoException {
+    public static Command readDeleteInput(String input) throws LebronException {
         String[] split = input.split(" ", 2);
 
         if (split.length != 2) {
-            throw new CheetoException("I need a task number to delete!");
+            throw new LebronException("I need a task number to delete!");
         } else if (split[1].trim().isEmpty()) {
-            throw new CheetoException("I need a task number to delete!");
+            throw new LebronException("I need a task number to delete!");
         }
 
         return new DeleteCommand(Integer.parseInt(split[1]) - 1);
